c57a8f038324bb38472e5c40a9f5000247aa02f0
RyanNolan1 <114003965+RyanNolan1@users.noreply.github.com>
2023-08-13 20:20:56 +0100

    this will get the dummy data from the myLibrary array displaying on the page in a basic table

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
-const theHobbit = new Book(
-  "The Hobbit",
-  "J.R.R Tolkien",
-  "295 pages",
-  "No"
-);
-theHobbit.info();
+// const theHobbit = new Book(
+//   "The Hobbit",
+//   "J.R.R Tolkien",
+//   "295 pages",
+//   "No"
+// );
+// theHobbit.info();
 
-let myLibary = [{title: "A Tale of Two Cities", author: "Charles Dickens", pages: "368", read: "No" }, {title: "Harry Potter and the Philosopher's Stone", author: "J. K. Rowling", pages: "223", read: "No" },  {title: "And Then There Were None", author: "Agatha Christie", pages: "272", read: "No" }];
+let myLibrary = [
+  {
+    title: "A Tale of Two Cities",
+    author: "Charles Dickens",
+    pages: "368",
+    read: "No",
+  },
+  {
+    title: "Harry Potter and the Philosopher's Stone",
+    author: "J. K. Rowling",
+    pages: "223",
+    read: "No",
+  },
+  {
+    title: "And Then There Were None",
+    author: "Agatha Christie",
+    pages: "272",
+    read: "No",
+  },
+];
 
 // function Book(title, author, pages, read) {
 //     this.title;
@@ -18,6 +37,28 @@ let myLibary = [{title: "A Tale of Two Cities", author: "Charles Dickens", pages
 //     };
 //   }
 
-function addBookToLibary() {
-
+const newTable = document.createElement("table");
+newTable.innerHTML =
+  "<thead><th>Title</th><th>Author</th><th>Pages</th><th>Read</th></thead>";
+for (let i = 0; i < myLibrary.length; i++) {
+  const newRow = document.createElement("tr");
+  const tdTitle = document.createElement("td");
+  const tdAuthor = document.createElement("td");
+  const tdPages = document.createElement("td");
+  const tdRead = document.createElement("td");
+  tdTitle.textContent = myLibrary[i].title;
+  tdAuthor.textContent = myLibrary[i].author;
+  tdPages.textContent = myLibrary[i].pages;
+  tdRead.textContent = myLibrary[i].read;
+  newRow.appendChild(tdTitle);
+  newRow.appendChild(tdAuthor);
+  newRow.appendChild(tdPages);
+  newRow.appendChild(tdRead);
+  newTable.appendChild(newRow);
 }
+
+const target = document.getElementById("target");
+target.appendChild(newTable);
+
+function addBookToLibrary() {}
+
