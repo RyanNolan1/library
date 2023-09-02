6301338aa6a3f4d74881e87797f40080c23e6c46
RyanNolan1 <114003965+RyanNolan1@users.noreply.github.com>
2023-09-02 07:06:35 +0100

    this will centre align the contents of the table

--- index.js
+++ index.js
@@ -57,6 +57,7 @@ function refreshLibrary() {
     tdPages.style.width = "10%";
     tdRead.style.width = "10%";
     tdCheckBox.style.width = "10%";
+    // tdCheckBox.style.align = center;
   }
 }
 
